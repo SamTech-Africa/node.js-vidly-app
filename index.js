41f2576ad4025a4ebb9021ca0030a32a948f25d4
Kinsammy <fanusamuel@gmail.com>
2024-03-09 19:08:39 +0100

    Restructure

--- index.js
+++ index.js
@@ -1,88 +1,10 @@
-const Joi = require("joi");
+const genres = require("./routes/genres");
 const express = require("express");
 
 const app = express();
 
-const vidlyUrl = "/vidly.com/api/genres";
-const vidlyUrlWithId = "/vidly.com/api/genres/:id";
-
 app.use(express.json());
-
-const genres = [
-  { id: 1, category: "Action" },
-  { id: 2, category: "Drama" },
-  { id: 3, category: "Comedy" },
-];
-
-// Get request for Genres List
-app.get(vidlyUrl, (req, res) => {
-  res.send(genres);
-});
-
-// Get request to get a genre by id
-app.get(vidlyUrlWithId, (req, res) => {
-  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
-
-  if (!genre)
-    return res.status(404).send(`Genre with id ${req.params.id} not found`);
-
-  res.send(genre);
-});
-
-// Post request for genre
-app.post(vidlyUrl, (req, res) => {
-  const { error } = validateGenre(req.body);
-
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  // Post Genre
-  const genre = {
-    id: genres.length + 1,
-    category: req.body.category,
-  };
-  genres.push(genre);
-
-  res.send(genre);
-});
-
-// Put request for genre to update genre
-app.put(vidlyUrlWithId, (req, res) => {
-  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
-
-  if (!genre)
-    return res.status(404).send(`Course with id: ${req.params.id} not found`);
-
-  const { error } = validateGenre(req.body);
-  if (error) return res.status(400).send(error.details[0].message);
-
-  // update genre
-  genre.category = req.body.category;
-  res.send(genre);
-});
-
-// Delete request to delete genre
-app.delete(vidlyUrlWithId, (req, res) => {
-  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
-
-  if (!genre)
-    return res.status(404).send(`Course with id: ${req.params.id} not found`);
-
-  const index = genres.indexOf(genre);
-  genres.splice(index, 1);
-
-  res.send(genre);
-});
-
-// ALL Functions
-function validateGenre(genre) {
-  const schema = Joi.object({
-    category: Joi.string().min(1).required(),
-  });
-
-  return schema.validate(genre);
-}
+app.use("/api/genres", genres);
 
 // PORT or ROUTE
 const port = process.env.PORT || 5000;
---
+++ routes/genres.js
@@ -0,0 +1,81 @@
+const express = require("express");
+const router = express.Router();
+const Joi = require("joi");
+
+const genres = [
+  { id: 1, category: "Action" },
+  { id: 2, category: "Drama" },
+  { id: 3, category: "Comedy" },
+];
+
+// Get request for Genres List
+router.get("/", (req, res) => {
+  res.send(genres);
+});
+
+// Get request to get a genre by id
+router.get("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
+
+  if (!genre)
+    return res.status(404).send(`Genre with id ${req.params.id} not found`);
+
+  res.send(genre);
+});
+
+// Post request for genre
+router.post("/", (req, res) => {
+  const { error } = validateGenre(req.body);
+
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  // Post Genre
+  const genre = {
+    id: genres.length + 1,
+    category: req.body.category,
+  };
+  genres.push(genre);
+
+  res.send(genre);
+});
+
+// Put request for genre to update genre
+router.put("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
+
+  if (!genre)
+    return res.status(404).send(`Course with id: ${req.params.id} not found`);
+
+  const { error } = validateGenre(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
+  // update genre
+  genre.category = req.body.category;
+  res.send(genre);
+});
+
+// Delete request to delete genre
+router.delete("/:id", (req, res) => {
+  const genre = genres.find((gen) => gen.id === parseInt(req.params.id));
+
+  if (!genre)
+    return res.status(404).send(`Course with id: ${req.params.id} not found`);
+
+  const index = genres.indexOf(genre);
+  genres.splice(index, 1);
+
+  res.send(genre);
+});
+
+// ALL Functions
+function validateGenre(genre) {
+  const schema = Joi.object({
+    category: Joi.string().min(1).required(),
+  });
+
+  return schema.validate(genre);
+}
+
+module.exports = router;

9fb2c96fc866dbb2122e0113cbfece1d53680130
Kinsammy <fanusamuel@gmail.com>
2024-03-15 17:56:16 +0100

    Authentication and Authorization

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 const { User, validate } = require("../models/user");
+const _ = require("lodash");
 const express = require("express");
 const router = express.Router();
 
@@ -8,14 +9,17 @@ router.post("/", async (req, res) => {
 
   if (error) return res.status(400).send(error.details[0].message);
 
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-  });
+  // Vaidate if the user is not registered or not
+  let user = await User.findOne({ email: req.body.email });
+  if (user) return res.status(400).send("User already registered.");
+
+  user = new User(_.pick(req.body, ["name", "email", "password"]));
 
   await user.save();
-  res.send(user);
+
+  const response = _.pick(user, ["id", "name", "email"]);
+
+  res.send(response);
 });
 
 module.exports = router;
